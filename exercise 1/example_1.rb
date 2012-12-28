eff60ff8d55de4c8c1545ec39bd7a635e0bbc29c
Meng Fung <meng.fung@gmail.com>
2012-12-27 19:48:38 -0800

    finished exercise 1

--- exercise 1/example_1.rb
+++ exercise 1/example_1.rb
@@ -1,5 +1,19 @@
 # sum of multiples of 3 and 5 to n
 
 def sum_multiples_of_3_or_5(target = 0)
-  3 + 5
+  sum = 0
+  (0...target).each do |number|
+    if multiple_of_3?(number) || multiple_of_5?(number) 
+      sum += number
+    end
+  end
+  sum
+end
+
+def multiple_of_3?(number)
+  number%3 == 0
+end
+
+def multiple_of_5?(number)
+  number%5 == 0
 end
--- exercise 1/example_1_spec.rb
+++ exercise 1/example_1_spec.rb
@@ -3,9 +3,33 @@ require 'rspec-given'
 require './example_1.rb'
 
 describe 'Multiples of 3 and 5' do
-  context "given an easy target of 5" do
+  context "given a target of 4" do
+    Given(:target) { 4 }
+    When(:sum) { sum_multiples_of_3_or_5(target) }
+    Then { expect(sum).to eql 3 }
+  end
+
+  context "given a target of 5" do
     Given(:target) { 5 }
     When(:sum) { sum_multiples_of_3_or_5(target) }
+    Then { expect(sum).to eql 3 }
+  end
+
+  context "given an easy target of 6" do
+    Given(:target) { 6 }
+    When(:sum) { sum_multiples_of_3_or_5(target) }
     Then { expect(sum).to eql 8 }
+  end
+
+  context "given an target of 7" do
+    Given(:target) { 7 }
+    When(:sum) { sum_multiples_of_3_or_5(target) }
+    Then { expect(sum).to eql 14 }
+  end
+
+  context "given an target of 10" do
+    Given(:target) { 10 }
+    When(:sum) { sum_multiples_of_3_or_5(target) }
+    Then { expect(sum).to eql 23 }
   end
 end
